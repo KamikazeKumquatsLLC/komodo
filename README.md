d6beaca7bdee69bd5e0f2f14cb6c5d5b69357e8e
Matt Horn <mathphreak@gmail.com>
2014-11-24 10:59:39 -0700

    set up Meteor on Windows
    Dang, this is complicated.

---
+++ README.md
@@ -0,0 +1,11 @@
+kapuut
+======
+
+Realtime classroom quizzing for power users.
+
+Development
+-----------
+
+    vagrant up
+    vagrant ssh
+    $ meteor
---
+++ init.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+if [ ! -d /home/vagrant/hamwerk ]; then
+    sudo apt-get install -qy curl
+    cd /home/vagrant
+    su vagrant -c 'curl -s https://install.meteor.com | sed s/--progress-bar/-s/ | sh'
+    su vagrant -c 'meteor create tmp'
+    cd tmp
+    echo "Creating empty Mongo stuff to fix permissions..."
+    su vagrant -c 'meteor' &
+    sleep 60
+    ps ax | grep meteor | awk '{print $1;}' | head -n -1 | xargs kill
+    #su vagrant -c 'meteor add accounts-password accounts-google email mquandalle:jade jquery less markdown underscore spiderable'
+    #su vagrant -c 'meteor add audit-argument-checks oauth-encryption mizzao:bootstrap-3 useraccounts:bootstrap'
+    #su vagrant -c 'meteor remove autopublish insecure'
+    echo "Configuring permission fix..."
+    echo "sudo mount --bind /home/vagrant/tmp/.meteor/local/ /vagrant/kapuut/.meteor/local/" >> /home/vagrant/.bashrc
+    echo "Making life easier..."
+    echo "cd /vagrant/kapuut" >> /home/vagrant/.bashrc
+    echo "Done!"
+fi
